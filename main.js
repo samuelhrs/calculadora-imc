4ad1f8db7cd222f6630ba6f97fa6c83e2fe7a7e3
Samuel Rocha <131828449+samuelhrs@users.noreply.github.com>
2023-05-08 21:16:08 -0300

    HTML & JAVASCRIPT OK
    Terminado de configurar o html e programar o javascript
    
    Só falta o CSS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const peso = document.getElementById('peso');
 const altura = document.getElementById('altura');
-const valorFinal = document.getElementById('valorFinal');
+
 
 
 function calcular_imc(){
@@ -8,5 +8,39 @@ function calcular_imc(){
 
    const imc = resultado.toFixed(1);
 
+   const exibirIMC = document.getElementById('resultado_imc');
+
+   if(imc < 18.5){
+   exibirIMC.innerHTML = (imc + ' | Você está abaixo do peso');
+
+   }
+   if(imc >= 18.6 && imc <= 24.9){
+      exibirIMC.innerHTML = (imc + ' | Você está no peso ideal');
+
+   }
+   if(imc >= 25 && imc <= 29.9){
+      exibirIMC.innerHTML = (imc + ' | Você está levemente acima do peso');
+
+   }
+   if(imc >= 30 && imc <= 34.9){
+      exibirIMC.innerHTML = (imc + ' | Você está com Obesidade de Grau I <p>Procure se exercitar</p>');
+
+   }
+   if(imc >= 35 && imc<= 39.9){
+      exibirIMC.innerHTML = (imc + ' | Você está com Obesidade de Grau II (Severa) <p>Procure se exercitar</p>');
+   }
+   else{
+      exibirIMC.innerHTML = (imc + ' | Você está com Obesidade de Grau III (Mórbida) <p>Procure um médico</p>');
+   
+   }
 }
 
+
+let reset = document.getElementById('reset');
+let inputs = document.querySelectorAll('input');
+
+reset.addEventListener('click', () => {
+      inputs.forEach(input => input.value = '');
+      const exibirIMC = document.getElementById('resultado_imc');
+      exibirIMC.innerHTML= "";
+})
